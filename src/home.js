083d6218485f36abeb99d59e78bcaee3d3ee4b97
wakoTheDev <wakopaul19@gmail.com>
2025-01-12 11:30:40 +0300

    update landing page

--- src/home.js
+++ src/home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./home.css"; 
 import MyHeader from "./homeheader";
+import BackgroundImage from "./binaryimage.jpg"
 
 const LandingPage = () => {
     const handleClick = () =>{
@@ -17,7 +18,7 @@ const LandingPage = () => {
       <main className="content">
         <h1 className="title">Welcome To BINARY TOOL</h1>
         <p className="description">
-            <p>Welcome to the BINARY TOOL, an indispensable asset for traders of digital options and CFDs. This state-of-the-art platform is designed to help you achieve precision in your trades by identifying sniper entries in the market. Utilizing cutting-edge technology, BINARY TOOL provides advanced market analysis, including volatility assessments, pattern sequences, and real-time market signals. Trusted by top options traders globally, this tool offers the insights you need to refine your trading strategies and maximize your potential for success. Always remember to trade responsibly, as the high risks associated with digital options and CFDs can result in significant losses.</p>
+            <p>Welcome to the Binary Tool, an indispensable asset for traders of digital options and CFDs. This state-of-the-art platform is designed to help you achieve precision in your trades by identifying sniper entries in the market. Utilizing cutting-edge technology, BINARY TOOL provides advanced market analysis, including volatility assessments, pattern sequences, and real-time market signals. Trusted by top options traders globally, this tool offers the insights you need to refine your trading strategies and maximize your potential for success. Always remember to trade responsibly, as the high risks associated with digital options and CFDs can result in significant losses.</p>
           Take your online and distance learning to new heights. Explore forex, Deriv,
           trading, analysis tools, and more with our professional courses.
         </p>
@@ -26,6 +27,10 @@ const LandingPage = () => {
         <div className="button-container">
           <button className="start-learning" onClick={handleClick}>Start Learning</button>
         </div>
+        <div className="image">
+        <img src={BackgroundImage}  alt={""} width={"100%"} height={"100%"} style={{objectFit:"fill"}} />
+        </div>
+        
       </main>
     </div>
   );
